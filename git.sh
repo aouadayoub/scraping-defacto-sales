0b604ead74d9318bd940d2cdb03f64e6b82e29d6
Ayoub <aouadayoub2003@gmail.com>
2023-12-05 01:19:49 +0100

    commit Tue Dec  5 01:19:49     2023

---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+start_time=$SECONDS
+git init
+git add .
+git commit -m "commit $(date)"
+git branch -M main
+git remote add origin https://github.com/aouadayoub/scraping-defacto-sales.git
+git push -u origin main
+elapsed=$(( SECONDS - start_time ))
+echo "Finish! $elapsed seconds to complete."
+
